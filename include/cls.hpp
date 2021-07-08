db85952470c34813e4a71b4368d624208ff0e361
fudepia <fudepia@outlook.jp>
2021-07-09 00:16:46 +0800

    Migrate cls.hpp

---
+++ include/cls.hpp
@@ -0,0 +1,21 @@
+#ifndef   CLS_HPP
+#define   CLS_HPP
+
+#include<bits/stdc++.h>
+
+#define di long unsigned int
+#define ull unsigned long long int
+#define ll long long int
+#define deg unsigned short
+
+using namespace std;
+
+#include"cls/structure/chain.hpp"
+#include"cls/carrier/linear.hpp"
+#include"cls/structure/field.hpp"
+#include"cls/structure/line.hpp"
+
+
+template<typename G> struct mono{};
+
+#endif
