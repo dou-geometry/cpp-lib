bc34d65ee3babbd23fcfda83be17ee1bcaf13ca4
fudepia <fudepia@outlook.jp>
2021-07-09 00:17:21 +0800

    Separate function from header file

--- include/conn/bash/stdout.hpp
+++ include/conn/bash/stdout.hpp
@@ -1,20 +1,6 @@
 #ifndef __CONN_BASH_O__
 #define __CONN_BASH_O__
 namespace d { namespace conn { namespace bash {
-
-std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
-}
-// https://stackoverflow.com/a/478960/8460574
-
+	std::string exec(const char*);
 }}}
 #endif
---
+++ lib/conn/bash/stdout.cpp
@@ -0,0 +1,15 @@
+#include"../../../../include/conn/bash/stdout.hpp"
+
+std::string d::conn::bash::exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+// https://stackoverflow.com/a/478960/8460574
