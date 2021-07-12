ff6a85ca2e74b6bde4fa408a38039222f39e358b
fudepia <fudepia@outlook.jp>
2021-07-12 21:54:41 +0800

    d::cov plotting example

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
-#include"../include/conn/bash/stdout.hpp"
+#include"../include/conn/sage/plot.hpp"
+#include"../include/cls.hpp"
 #include<string>
 #include<iostream>
 
 int main() {
-	std::string  o=d::conn::bash::exec("ls");
-	std::cout << o << std::endl;
+	d::conn::sage::settings::files x("/tmp/plot.png");
+	d::cov f(d::coord<int>({12, 24}));
+	std::cout << f.IDzuCoord(f.coordZuID(d::coord<double>({2, 0}))) << std::endl;
+	std::cout << f.multiplier << std::endl;
+	std::cout << x << std::endl;
+	f[10]=true;
+	std::cout << d::conn::sage::plot(f, x) << std::endl;
 	return 0;
 }
