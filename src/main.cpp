3f055e1cc58e18971334909c40528ae4e3b47473
fudepia <fudepia@outlook.jp>
2021-07-12 22:03:16 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,8 @@
 int main() {
 	d::conn::sage::settings::files x("/tmp/plot.png");
 	d::cov f(d::coord<int>({12, 24}));
-	std::cout << f.IDzuCoord(f.coordZuID(d::coord<double>({2, 0}))) << std::endl;
-	std::cout << f.multiplier << std::endl;
 	std::cout << x << std::endl;
-	f[10]=true;
+	f[20]=true;
 	std::cout << d::conn::sage::plot(f, x) << std::endl;
 	return 0;
 }
