f0f155ff715106ba52d510fc2aa85f31cafc2bd8
fudepia <fudepia@outlook.jp>
2021-07-13 23:56:23 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
+#include"../include/conn/sage/plot.hpp"
 #include"../include/cls.hpp"
 #include<string>
 #include<iostream>
 
-int main(int argv, char **argv) {
+int main(int argc, char **argv) {
     d::cov f(d::coord<double>({12, 12}));
-    d::line<double> l(d::coord<double>({1, 1}), d::coord<double>({1, 2}));
+    d::line<double> l(d::coord<double>({1,1}), d::coord<double>({1, 2}));
     std::cout << f.calc(l) << std::endl;
 	return 0;
 }
