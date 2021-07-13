95f30e77b1fde046f3457aae7e6bb148b6a068d6
fudepia <fudepia@outlook.jp>
2021-07-13 22:15:19 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
-#include"../include/conn/sage/plot.hpp"
 #include"../include/cls.hpp"
 #include<string>
 #include<iostream>
 
 int main(int argv, char **argv) {
-	d::conn::sage::settings::files x("/tmp/plot.png");
-	d::cov f(d::coord<int>({12, 24}));
-	std::cout << x << std::endl;
-	f[20]=true;
-	std::cout << d::conn::sage::plot(f, x) << std::endl;
+    d::cov f(d::coord<double>({12, 12}));
+    d::line<double> l(d::coord<double>({1, 1}), d::coord<double>({1, 2}));
+    std::cout << f.calc(l) << std::endl;
 	return 0;
 }
