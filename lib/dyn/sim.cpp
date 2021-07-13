78cf47cad94d2bb60517d3e75cb4bc9b38258bb6
fudepia <fudepia@outlook.jp>
2021-07-13 22:15:16 +0800

    Create sim.cpp

---
+++ lib/dyn/sim.cpp
@@ -0,0 +1,6 @@
+#include"../../include/cls/carrier/linear.hpp"
+#include"../../include/cls/structure/field.hpp"
+#include"../../include/numerical/integration.hpp"
+
+template<typename T>
+mono<T> calc(mono<T> m, field<T> f)
