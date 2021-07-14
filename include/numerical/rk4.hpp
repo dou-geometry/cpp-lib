c82a752c697b3d79f7687d4c17f656724e6c7c04
fudepia <fudepia@outlook.jp>
2021-07-14 21:35:21 +0800

    Update rk4.hpp

--- include/numerical/rk4.hpp
+++ include/numerical/rk4.hpp
@@ -1,7 +1,9 @@
 #ifndef __CLS_NUM_RK4__
 #define __CLS_NUM_RK4__
+#include"../cls/mono.hpp"
 
-namespace d::numerical {
+namespace d::numerical::rk4 {
+    template<typename C> coord<C> next();
 }
 
 #endif
