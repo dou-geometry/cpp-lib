5d0dd3e1536ca5e745834eea2492b7abddfcf186
fudepia <fudepia@outlook.jp>
2021-07-14 17:02:50 +0800

    Update line.hpp

--- include/cls/line.hpp
+++ include/cls/line.hpp
@@ -7,9 +7,10 @@ struct line {
     d::coord<L> s, t, d;
     L length;
     line(d::coord<L> a, d::coord<L> b): s(a), t(b), d((b-a).unit()), length((b-a).norm()) {}
+    line(const line<L> &other): s(other.d), t(other.t), d(other.d), length(other.length) {}
+    //line(line<T> &&other) noexcept: d(std::exchange(other.d, nullptr)), dim(std::exchange(other.dim,(di)0)) {}
     ~line() {
-        s.~coord();
-        t.~coord();
+        // perhaps we don't need destructor
     }
     double dist(const d::coord<L> Punkt) {
         double t=(Punkt-s).dot(d);
