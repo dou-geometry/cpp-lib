7da01b34a674c5768fe84d8ec7f9546be088c331
fudepia <fudepia@outlook.jp>
2021-07-15 00:02:27 +0800

    Update rk4.cpp
    First Implementation according to [doc](https://dou-geometry.github.io/cpp-lib/cpp)

--- lib/numerical/rk4.cpp
+++ lib/numerical/rk4.cpp
@@ -1,1 +1,13 @@
 #include"../../include/numerical/rk4.cpp
+// Explicit Instantiation
+namespace d::numerical::rk4 {
+    template coord<float> next();
+    template coord<double> next();
+    template coord<long double> next();
+    template coord<double double> next();
+}
+
+// Template Implementation
+template<typename C>
+coord<C> next() {
+}
