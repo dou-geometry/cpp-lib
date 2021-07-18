a398bc6604e57289233a3c7ec9a920461847d782
fudepia <fudepia@outlook.jp>
2021-07-18 21:37:34 +0800

    Update rk4.hh

--- lib/numerical/rk4.hh
+++ lib/numerical/rk4.hh
@@ -1,12 +1,16 @@
 #pragma once
 
+#include"../cls/coord.hh"
 #include"../dyn/mono.hh"
+#include<functional>
 
 // RK4 currently only support 2-order ODE
 
 namespace d::numerical::rk4 {
     const double h=1e-4;
-    template<typename C> d::coord<C> run();
+    template<bool runForever=false, typename C, typename R, typename F>
+        requires std::convertible_to<R, d::coord<C>> && std::convertible_to<F, std::function<d::coord<C>(d::coord<C>)>>
+        R run(R cur, const F &f, const double tt=0);
     template<typename C> d::dyn::mono<C> next();
 }
 
