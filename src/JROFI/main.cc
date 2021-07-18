d85fb7022ce2007a407766b6c39a7b7ae95d7f00
fudepia <fudepia@outlook.jp>
2021-07-18 21:37:30 +0800

    Update main.cc

--- src/JROFI/main.cc
+++ src/JROFI/main.cc
@@ -1,17 +1,17 @@
 #include"../lib/numerical/rk4.hh"
 #include <cmath>
+#include<iostream>
 
 double gaussianFunc(double x) { return std::exp(-1*pow(x,2)); }
 double gaussianFunc(d::coord<double> x) {
     return std::exp(-1*pow(x.norm(), 2));
 }
 
-d::coord<double> f(d::coord<double> x) {
-    // The field function
-    return d::coord<double>({gaussianFunc(x), 0});
-}
 
 int main() {
-    d::dyn::mono<double, true> m;
-    m=d::numerical::rk4(m, f);
+    auto f=[](d::coord<double> x) { return d::coord<double>({gaussianFunc(x), 0}); };
+    d::dyn::mono<double, true> m(2);
+    std::function<d::coord<double>(d::coord<double>)> cf=f;
+    m=d::numerical::rk4::run<false, double>(m, f, 10.0);
+    std::cout << m << std::endl;
 }
