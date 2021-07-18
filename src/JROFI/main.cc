29225daea5a512407493c62f16428532807c42d7
fudepia <fudepia@outlook.jp>
2021-07-18 22:38:08 +0800

    Solution 1
    This solves #9 but with a cost of more memory occupied per `d::coord`, which might seem small but consider when opening an array of it. If each one has a self-reference is kinda hurting.

--- src/JROFI/main.cc
+++ src/JROFI/main.cc
@@ -13,8 +13,8 @@ d::coord<double> f(d::coord<double> x) {
 }
 
 int main() {
-    d::dyn::mono<double, true> m(2);
-    //d::coord<double> m(2);
+    //d::dyn::mono<double, true> m(2);
+    d::coord<double> m(2);
     std::function<d::coord<double>(d::coord<double>)> cf=f;
     m=d::numerical::rk4::run(m, f, 10.0);
     std::cout << m << std::endl;
