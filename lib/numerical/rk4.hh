96697a9e9d37d52b52c571ebee4aba232f837e87
fudepia <fudepia@outlook.jp>
2021-07-18 23:05:59 +0800

    Patch: forget to remove previous temp-fix (self-ref in `d::coord`)

--- lib/numerical/rk4.hh
+++ lib/numerical/rk4.hh
@@ -8,14 +8,22 @@
 
 namespace d::numerical::rk4 {
     const double h=1e-4;
+    template<typename R>
+        concept tracksTime = requires(const R &r) { r.t; };
     template<typename R, typename F>
-        concept x = requires(const R &r, const F &f) {
+        concept x1 = requires(const R &r, const F &f) {
             std::convertible_to<F, std::function<typename std::decay<decltype(r.pos)>::type(typename std::decay<decltype(r.pos)>::type)>>;
             {f(r)}->std::same_as<typename std::decay<decltype(r.pos)>::type>;
             std::convertible_to<R, typename std::decay<decltype(r.pos)>::type>;
         };
-    template<typename R>
-        concept tracksTime = requires(const R &r) { r.t; };
+    template<typename R, typename F>
+        concept x2 = requires(const R &r, const F &f) {
+            std::convertible_to<F, std::function<typename std::decay<decltype(r)>::type(typename std::decay<decltype(r)>::type)>>;
+            {f(r)}->std::same_as<typename std::decay<decltype(r)>::type>;
+            std::convertible_to<R, typename std::decay<decltype(r)>::type>;
+        };
+    template<typename R, typename F>
+        concept x = (x1<R, F> || x2<R, F>);
     template<bool runForever=false, typename R, typename F>
         requires x<R, F>
         R run(R cur, const F &f, const double tt=0);
