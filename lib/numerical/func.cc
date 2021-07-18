67a6dec163c54a2c8744db7e5e0cf2c1a1c980ac
fudepia <fudepia@outlook.jp>
2021-07-18 18:20:04 +0800

    Separate template implementation

--- lib/numerical/func.cc
+++ lib/numerical/func.cc
@@ -6,9 +6,3 @@ namespace d::numerical::corrfunc {
     template long double linear(const func<long double>&, const coord<long double>&);
     template __float128 linear(const func<__float128>&, const coord<__float128>&);
 }
-
-template<typename T>
-T linear(const func<T>& f, const coord<T>& crd) {
-    // auto [a, b] = f.neighbour(crd);
-    auto h = f.hidari(crd);
-}
