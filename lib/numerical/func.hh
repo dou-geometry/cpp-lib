349100c2680259a889e32d282a49f09d242cad20
fudepia <fudepia@outlook.jp>
2021-07-18 19:19:22 +0800

    Update d::numerical::func

--- lib/numerical/func.hh
+++ lib/numerical/func.hh
@@ -1,23 +1,29 @@
 #pragma once
-#include"../cls/field.hpp"
+#include"../cls/field.hh"
+#include<functional>
 #include<utility>
-#include<concept>
+#include<concepts>
+#include<type_traits>
 namespace d::numerical {
 
+template<typename T>
+requires std::signed_integral<T> || std::floating_point<T> // As we use unsign/sign coordinates to determine whether or not it's dL or dLinversed;
+struct func;
+
 namespace corrfunc {
-    template<typename T> T linear(const func<T>&, const coord<T>&);
+    template<typename T> T linear(const d::numerical::func<T>&, const d::coord<T>&);
 }
 
 template<typename T>
-require std::signed_integral<T> // As we use unsign/sign coordinates to determine whether or not it's dL or dLinversed;
+requires std::signed_integral<T> || std::floating_point<T> // As we use unsign/sign coordinates to determine whether or not it's dL or dLinversed;
 struct func:field<T> {
     const di dim;
     const coord<T> einPunkt;
     const coord<T> dLi;
-    T (*correction)(const coord<T>& crd) const = &(corrfunc::linear);
+    T (*correction)(const coord<T>& crd) = &(corrfunc::linear);
     func(): dim(1), einPunkt(1, (T)0), dLi(1, (T)1) {}
     func(di d): dim(d), einPunkt(d, (T)0), dLi(d, (T)1) {};
-    func(coord<di> i): dim(i.dim), einPunkt(d.dim, (T)0), dLi(i) {};
+    func(coord<di> i): dim(i.dim), einPunkt(i.dim, (T)0), dLi(i) {};
     func(coord<T> d): dim(d.dim), einPunkt(d.dim, (T)0), dLi(d.inv()) {}
     func(coord<T> d, coord<T> eP): dim(d.dim), einPunkt(eP), dLi(d.inv()) {}
     func(coord<di> d, coord<T> eP): dim(d.dim), einPunkt(eP), dLi(d) {}
@@ -26,16 +32,20 @@ struct func:field<T> {
             field<T>::d[i]=f(field<T>::IDzuCoord(i));
         }
     }
-    template<typename...Ts> requires convertible_to<Ts, di> T& operator[](Ts...) {
-        constexpr di n = sizeof...(T);
+    template<typename...Ts> requires convertible_to<std::common_type_t<Ts...>, di> T& operator[](Ts...) {
+        constexpr di n = sizeof...(Ts);
     }
     T& operator[](int i) {
+        return (*this)[0];
     };
     T operator[](int i) const {
+        return (*this)[0];
     }
-    template<typename C> C operator()(coord<C>) {
+    template<typename C> C operator()(coord<C> crd) {
+        return crd;
     }
-    template<typename C> coord<T> hidari(const coord<C>& crd) {
+    template<typename C> coord<T> hidari(const coord<C>& crd) const {
+        return crd;
     }
     template<typename C> std::pair<coord<T>, coord<T>> neighbour(const coord<C>& crd) {
         return std::make_pair(this->hidari(crd), this->migi(crd));
