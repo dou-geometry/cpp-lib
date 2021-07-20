549d056821dd6bb058dfdd1c176f62628373898c
fudepia <fudepia@outlook.jp>
2021-07-21 00:18:41 +0800

    Update

--- lib/dou/geo/rk4.hh
+++ lib/dou/geo/rk4.hh
@@ -12,9 +12,9 @@ namespace d::dou::geo::rk4 {
         concept tracksTime = requires(const R &r) { r.t; };
     template<typename R, typename F>
         concept x1 = requires(const R &r, const F &f) {
-            std::convertible_to<F, std::function<typename std::decay<decltype(r.pos)>::type(typename std::decay<decltype(r.pos)>::type)>>;
-            {f(r)}->std::same_as<typename std::decay<decltype(r.pos)>::type>;
-            std::convertible_to<R, typename std::decay<decltype(r.pos)>::type>;
+            std::convertible_to<F, std::function<typename std::decay<decltype(r[0])>::type(typename std::decay<decltype(r[0])>::type)>>;
+            {f(r)}->std::same_as<typename std::decay<decltype(r[0])>::type>;
+            std::convertible_to<R, typename std::decay<decltype(r[0])>::type>;
         };
     template<typename R, typename F>
         concept x2 = requires(const R &r, const F &f) {
