445074a33ddb0520dfb09cc86852234194d4d15f
fudepia <fudepia@outlook.jp>
2021-07-21 00:18:55 +0800

    Update pendulum-ang.cc

--- src/test/pendulum-ang.cc
+++ src/test/pendulum-ang.cc
@@ -1,4 +1,4 @@
-#include"../lib/numerical/rk4.hh"
+#include"../lib/dou/geo/rk4.hh"
 #include <cmath>
 #include<iostream>
 
@@ -8,11 +8,29 @@
  * Angle-verion
  * Parameter: [r, ang]
  */
+template<typename R, typename F>
+concept x1 = requires(const R &r, const F &f) {
+    std::convertible_to<F, std::function<typename std::decay<decltype(r[0])>::type(typename std::decay<decltype(r[0])>::type)>>;
+    {f(r)}->std::same_as<typename std::decay<decltype(r[0])>::type>;
+    std::convertible_to<R, typename std::decay<decltype(r[0])>::type>;
+};
+template<typename R, typename F>
+concept x2 = requires(const R &r, const F &f) {
+    std::convertible_to<F, std::function<typename std::decay<decltype(r)>::type(typename std::decay<decltype(r)>::type)>>;
+    {f(r)}->std::same_as<typename std::decay<decltype(r)>::type>;
+    std::convertible_to<R, typename std::decay<decltype(r)>::type>;
+};
+
+void test(d::dyn::mono<double, true> a) {
+    return;
+}
 
 int main() {
-    auto a=[](d::coord<double> x) { return -G/x[0]*std::sin(x[1]); };
-    d::dyn::mono<double, true> m(2);
-    m.pos=d::coord<double>({2, 0});
-    m=d::numerical::rk4::run(m, a, 10.0);
-    std::cout << m << std::endl;
+    auto a=[](d::coord<double> x) { return d::coord<double>({0, -G/x[0]*std::sin(x[1])}); };
+    d::coord<double> d({1, 2}), b({2, 4}), c({2, 0});
+    d::dyn::mono<double, true> m(2, d, b, c);
+    test(m);
+    std::cout << ":::" << std::endl;
+    std::cout << m.order << std::endl;
+    std::cout << m[0] << std::endl;
 }
