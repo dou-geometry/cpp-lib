4a41cafd709fe10ffdd1e1763033638de04731ac
fudepia <fudepia@outlook.jp>
2021-07-21 01:20:45 +0800

    Fix namespace

--- lib/dyn/mono.hh
+++ lib/dyn/mono.hh
@@ -1,7 +1,6 @@
 #pragma once
 
 #include"../cls/coord.hh"
-#include"../concepts/nonDim.hh"
 #include<iostream>
 #include <type_traits>
 #include<cassert>
@@ -14,13 +13,13 @@ namespace d::dyn {
             di order;
             double t;
             mono* log=nullptr;
-            coord<T> *d=nullptr;
-            //coord<T> &pos=d[0], &vel=d[1], &acc=d[2];
+            d::coord<T> *d=nullptr;
+            //d::coord<T> &pos=d[0], &vel=d[1], &acc=d[2];
             template<typename...Ts>
-                requires std::same_as<coord<T>, typename std::common_type<Ts...>::type>
+                requires std::same_as<d::coord<T>, typename std::common_type<Ts...>::type>
                 mono(di ord, Ts...args): order(ord), t(0) {
                     std::cout << "Standard init d::dyn::mono" << std::endl;
-                    d=new coord<T>[order+1];
+                    d=new d::coord<T>[order+1];
                     assert(sizeof...(Ts)<=(order+1)&& "d::dyn::mono initialization error, too many arguments");
                     di i=0;
                     (...,void(d[i++] = args)); // https://stackoverflow.com/a/34569679/8460574
@@ -29,21 +28,21 @@ namespace d::dyn {
                     for(di i=0; i<=order; i++)
                         std::cout << d[i].d << std::endl;
                 }
-            mono(): mono(1, coord<T>(1)) {
+            mono(): mono(1, d::coord<T>(1)) {
                 std::cout << "empty mono" << std::endl;
             }
-            mono(di dimension): mono(2, coord<T>(dimension)) {}
+            mono(di dimension): mono(2, d::coord<T>(dimension)) {}
 //            template<typename...Ts>
-//                requires std::same_as<coord<T>, typename std::common_type<Ts...>::type>
+//                requires std::same_as<d::coord<T>, typename std::common_type<Ts...>::type>
 //                mono(Ts...args): mono(sizeof...(Ts)-1, args..., 0.) {}
 //            template<typename X, typename...Ts> 
-//                requires std::same_as<coord<T>, typename std::common_type<Ts...>::type> && d::nonDim<X>
+//                requires std::same_as<d::coord<T>, typename std::common_type<Ts...>::type> && d::nonDim<X>
 //                mono(X it, Ts...args): mono(sizeof...(Ts)-1, args..., it) {}
 //            template<typename X, typename...Ts> 
-//                requires std::same_as<coord<T>, typename std::common_type<Ts...>::type> && d::nonDim<X>
+//                requires std::same_as<d::coord<T>, typename std::common_type<Ts...>::type> && d::nonDim<X>
 //                mono(X it, di ord, Ts...args): mono(ord, args..., it) {}
 //            template<typename X, typename...Ts> 
-//                requires std::same_as<coord<T>, typename std::common_type<Ts...>::type> && d::nonDim<X>
+//                requires std::same_as<d::coord<T>, typename std::common_type<Ts...>::type> && d::nonDim<X>
 //                mono(di ord, X it, Ts...args): mono(ord, args..., it) {}
             ~mono() {
                 std::cout << "------------" << std::endl;
@@ -57,8 +56,8 @@ namespace d::dyn {
 
             mono(const mono<T> &other): order(other.order), t(other.t) {
                 std::cout << "&()" << std::endl;
-                d=new coord<T>[order+1];
-                if constexpr(other.d!=nullptr) memcpy(d, other.d, sizeof(coord<T>)*(order+1));
+                d=new d::coord<T>[order+1];
+                if constexpr(other.d!=nullptr) memcpy(d, other.d, sizeof(d::coord<T>)*(order+1));
             }
             mono(mono<T> &&other) noexcept: d(std::exchange(other.d, nullptr)), order(std::exchange(other.order, 0)), t(std::exchange(other.t, 0)), log(std::exchange(other.log, nullptr)) {
                 std::cout << "&&()" << std::endl;
@@ -70,8 +69,8 @@ namespace d::dyn {
                 // Only copying t, pos, vel
                 t=other.t; // As order in array are only relative to the log[0]
                 order=other.order;
-                d=new coord<T>[order+1];
-                memcpy(d, other.d, sizeof(coord<T>)*(order+1));
+                d=new d::coord<T>[order+1];
+                memcpy(d, other.d, sizeof(d::coord<T>)*(order+1));
                 return *this;
             }
             mono& operator=(mono<T> &&other) noexcept {
@@ -82,10 +81,10 @@ namespace d::dyn {
                 swap(d, other.d);
                 return *this;
             }
-            coord<T>& operator[](int i) {
+            d::coord<T>& operator[](int i) {
                 return this->d[i];
             }
-            coord<T> operator[](int i) const {
+            d::coord<T> operator[](int i) const {
                 return this->d[i];
             }
 //            mono& operator()(double t) { // This preforms check if t matches, if failed it'll be followed by binary search
@@ -108,7 +107,7 @@ namespace d::dyn {
                 os << "t="<<x.t<<"; pos="<<x[0]<<"; vel="<<x[1];
                 return os;
             }
-            //template<typename C> operator d::coord<C>() const { return pos; }
+            //template<typename C> operator d::d::coord<C>() const { return pos; }
             template<typename C> operator d::coord<C>() const { return this->d[0]; }
         };
 
