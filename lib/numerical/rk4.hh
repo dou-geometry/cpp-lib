80587bd65b1736ee3959837c6fbbb19eb432989f
fudepia <fudepia@outlook.jp>
2021-07-21 23:58:23 +0800

    Update rk4.*

--- lib/numerical/rk4.hh
+++ lib/numerical/rk4.hh
@@ -11,22 +11,14 @@ namespace d::numerical::rk4 {
     template<typename R>
         concept tracksTime = requires(const R &r) { r.t; };
     template<typename R, typename F>
-        concept x1 = requires(const R &r, const F &f) {
-            std::convertible_to<F, std::function<typename std::decay<decltype(r.pos)>::type(typename std::decay<decltype(r.pos)>::type)>>;
-            {f(r)}->std::same_as<typename std::decay<decltype(r.pos)>::type>;
-            std::convertible_to<R, typename std::decay<decltype(r.pos)>::type>;
+        concept dynMono = requires(const R &r, const F &f) {
+            std::convertible_to<F, std::function<typename std::decay<decltype(r[0])>::type(R)>>;
+            {f(r)}->std::same_as<typename std::decay<decltype(r[0])>::type>;
+            std::convertible_to<R, typename std::decay<decltype(r[0])>::type>;
         };
-    template<typename R, typename F>
-        concept x2 = requires(const R &r, const F &f) {
-            std::convertible_to<F, std::function<typename std::decay<decltype(r)>::type(typename std::decay<decltype(r)>::type)>>;
-            {f(r)}->std::same_as<typename std::decay<decltype(r)>::type>;
-            std::convertible_to<R, typename std::decay<decltype(r)>::type>;
-        };
-    template<typename R, typename F>
-        concept x = (x1<R, F> || x2<R, F>);
     template<bool runForever=false, typename R, typename F>
-        requires x<R, F>
-        R run(R cur, const F &f, const double tt=0);
+        requires dynMono<R, F>
+        R run(R& cur, const F &f, const double tt=0);
     template<typename C> d::dyn::mono<C> next();
 }
 
