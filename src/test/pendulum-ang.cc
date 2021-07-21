d5c5870bf5b9eb12f44a19e1b946393ca53e10b0
fudepia <fudepia@outlook.jp>
2021-07-21 13:12:33 +0800

    Update pendulum-ang.cc

--- src/test/pendulum-ang.cc
+++ src/test/pendulum-ang.cc
@@ -10,14 +10,12 @@
  * Angle-verion
  * Parameter: [r, ang]
  */
-void test(d::dyn::mono<double, true> a) {
-    return;
-}
 
 int main() {
     auto a=[](d::coord<double> x) { return d::coord<double>({0, -G/x[0]*std::sin(x[1])}); };
     d::coord<double> d({1, 2}), b({2, 4}), c({2, 0});
-    d::dyn::mono<double, true> m(2, d, b, c);
-    test(m);
+    d::dyn::mono<double, true> m(2ul, 0.0, d, b, c);
+    std::cout << m << std::endl;
+    m.shift<-1>(c);
     std::cout << m << std::endl;
 }
