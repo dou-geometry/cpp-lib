69a5c54c8b1627b7ac68f82aedcd6152573fcb1f
fudepia <fudepia@outlook.jp>
2021-07-22 10:54:52 +0800

    Update pendulum-ang.cc

--- src/test/pendulum-ang.cc
+++ src/test/pendulum-ang.cc
@@ -18,10 +18,12 @@ int main() {
     auto a=[](const d::dyn::mono<double, true>& m) { auto x=m[0]; return d::coord<double>({0, -G/x[0]*std::sin(x[1])}); };
     d::coord<double> initPos({2, 125.0/180.0*M_PI});
     d::dyn::mono<double, true> m(2, initPos, d::coord<double>(2), d::coord<double>(2));
-    d::numerical::rk4::run<10, false>(m, a, 1.2);
+    d::numerical::rk4::run<100000, false>(m, a, 10000);
     std::cout << "Data: " << m.logSize << std::endl;
-    d::conn::sage::settings::files<d::conn::sage::settings::gif> sett;
-    std::cout << sett << std::endl;
-    std::cout << d::conn::sage::anime(m, sett);
+    d::conn::sage::settings::files<d::conn::sage::settings::gif> anim;
+    d::conn::sage::settings::files<d::conn::sage::settings::png> gph;
+    //std::cout << d::conn::sage::anime(m, anim);
+    std::cout << d::conn::sage::plot(m, gph);
+    std::cout << "Animation:\n"<<anim<<"\nPlot:\n"<<gph<<std::endl;
     return 0;
 }
