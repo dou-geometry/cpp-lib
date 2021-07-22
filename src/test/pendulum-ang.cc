ac41d59f8ddd4ff3e66291636d544339a550db0d
fudepia <fudepia@outlook.jp>
2021-07-23 00:57:41 +0800

    Change to large h, quicker for debugging

--- src/test/pendulum-ang.cc
+++ src/test/pendulum-ang.cc
@@ -1,6 +1,6 @@
-#include"../lib/numerical/rk4.hh"
 #include"../lib/cls/coord.hh"
 #include"../lib/dyn/mono.hh"
+#include"../lib/numerical/rk4.hh"
 #include"../lib/conn/sage/settings.hh"
 #include"../lib/conn/sage/plot.hh"
 #include <cmath>
@@ -18,12 +18,12 @@ int main() {
     auto a=[](const d::dyn::mono<double, true>& m) { auto x=m[0]; return d::coord<double>({0, -G/x[0]*std::sin(x[1])}); };
     d::coord<double> initPos({2, 125.0/180.0*M_PI});
     d::dyn::mono<double, true> m(2, initPos, d::coord<double>(2), d::coord<double>(2));
-    d::numerical::rk4::run<520, false>(m, a, 1224);
+    d::numerical::rk4::run<520, false>(m, a, 1224, 0.1);
     std::cout << "Data: " << m.logSize << std::endl;
     d::conn::sage::settings::files<d::conn::sage::settings::gif> anim;
     d::conn::sage::settings::files<d::conn::sage::settings::png> gph;
     //std::cout << d::conn::sage::anime(m, anim);
-    std::cout << d::conn::sage::plot(m, gph);
+    //std::cout << d::conn::sage::plot(m, gph);
     std::cout << "Animation:\n"<<anim<<"\nPlot:\n"<<gph<<std::endl;
     return 0;
 }
