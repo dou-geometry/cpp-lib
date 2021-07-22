0aaf459d6cd146b3e62897ea46426415ba26cdbc
fudepia <fudepia@outlook.jp>
2021-07-22 10:56:41 +0800

    Update pendulum-ang.cc

--- src/test/pendulum-ang.cc
+++ src/test/pendulum-ang.cc
@@ -18,7 +18,7 @@ int main() {
     auto a=[](const d::dyn::mono<double, true>& m) { auto x=m[0]; return d::coord<double>({0, -G/x[0]*std::sin(x[1])}); };
     d::coord<double> initPos({2, 125.0/180.0*M_PI});
     d::dyn::mono<double, true> m(2, initPos, d::coord<double>(2), d::coord<double>(2));
-    d::numerical::rk4::run<100000, false>(m, a, 10000);
+    d::numerical::rk4::run<520, false>(m, a, 1224);
     std::cout << "Data: " << m.logSize << std::endl;
     d::conn::sage::settings::files<d::conn::sage::settings::gif> anim;
     d::conn::sage::settings::files<d::conn::sage::settings::png> gph;
