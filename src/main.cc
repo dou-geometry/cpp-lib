17d1db8d967d41fc522c67d52c1d952f6738dd20
fudepia <fudepia@outlook.jp>
2021-07-25 14:08:28 +0800

    Fin. d::ring

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,9 @@
-#include"../lib/numerical/integration.hh"
+#include"../lib/cls/ring.hh"
 #include<string>
 #include<iostream>
 
 int main(int argc, char **argv) {
-    auto f=[](d::coord<double> x) { return sin(x[0]); };
-    std::function<double(d::coord<double>)> ff=f;
-    d::line<double> l(d::coord<double>({0}), d::coord<double>({1}));
-    std::cout << d::numerical::integrate(l, ff) << std::endl;
+    d::ring x(12.24);
+    std::cout << x << std::endl;
 	return 0;
 }
