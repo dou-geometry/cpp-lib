217084d3ac34feb1951e3b47a8f161afd5d6b1f5
fudepia <fudepia@outlook.jp>
2021-07-25 14:54:16 +0800

    Fix concept to constexpr-if

--- lib/manip/rotate.hh
+++ lib/manip/rotate.hh
@@ -1,19 +1,17 @@
 #pragma once
-#include"../../concepts/coordCarrier.hh"
-#include"../../concepts/dim2.hh"
-#include"../../cls/matrix.hh"
+#include"../concepts/coordCarrier.hh"
+#include"../concepts/dim2.hh"
+#include"../cls/matrix.hh"
 
 namespace d {
     template<typename T>
-        requires d::coordCarrier<T> && d::dim2<T>
-        T rotate(T x, double theta) {
-            for(di i=0; i<x.dim; ++i)
-                d::rotate(x.d[i], theta);
-            return x;
-        };
-    template<typename T>
-        requires (!(d::coordCarrier<T>)) && d::dim2<T>
+        requires d::dim2<T>
         inline T rotate(T x, double theta) {
-            return d::matrix::R(theta)*x;
+            if constexpr(d::coordCarrier<T>) {
+                for(di i=0; i<x.dim; ++i)
+                    x[i]=d::matrix<double>::R(theta)*x[i];
+                return x;
+            } else
+                return d::matrix<double>::R(theta)*x;
         };
 }
--- lib/manip/translate.hh
+++ lib/manip/translate.hh
@@ -1,17 +1,15 @@
 #pragma once
-#include"../../cls/coord.hh"
+#include"../concepts/coordCarrier.hh"
 
 namespace d {
     template<typename T>
-        requires d::coordCarrier<T>
         T translate(T x, const d::coord<double>& delta) {
-            for(di i=0; i<x.dim; ++i)
-                d::translate(x.d[i], delta);
-            return x;
-        };
-    template<typename T>
-        requires (!(d::coordCarrier<T>))
-        inline T translate(T x, const d::coord<double>& delta) {
-            return x+delta;
+            if constexpr(d::coordCarrier<T>) {
+                for(di i=0; i<x.dim; ++i)
+                    d::translate(x.d[i], delta);
+                return x;
+            } else {
+                return x+delta;
+            }
         };
 }
