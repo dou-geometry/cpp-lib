438466aaed3247e2bdb731722c177c9c3e842eb8
fudepia <fudepia@outlook.jp>
2021-07-28 18:42:05 +0800

    Add Conditional terminated function test

---
+++ src/test/condTerRK4.cc
@@ -0,0 +1,12 @@
+#include"../lib/cls/coord.hh"
+#include"../lib/dyn/mono.hh"
+#include"../lib/numerical/rk4.hh"
+#include"../lib/conn/sage/settings.hh"
+#include"../lib/conn/sage/plot.hh"
+#include <cmath>
+#include<iostream>
+#include<string>
+
+int main() {
+    return 0;
+}
