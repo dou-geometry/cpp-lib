4d39c55b0914db4b7fed06d6aafb0a76b3839ae2
fudepia <fudepia@outlook.jp>
2021-07-29 23:45:45 +0800

    Change from d::dyn::mono to d::dou::mono for additional existence member

--- lib/dou/sys.hh
+++ lib/dou/sys.hh
@@ -4,13 +4,13 @@
 
 namespace d::dou {
     struct sys2d { //VLA version
-        d::dyn::mono<double>* d;
+        d::dou::mono<double>* d;
         di amount;
         const di dim=2;
         sys2d() = delete;
         sys2d(di cnt, double leftBoundary=-2., double rightBoundary=2.): amount(cnt) {
-            //d=new d::dyn::mono<double>[amount];
-            d=(d::dyn::mono<double>*)malloc(sizeof(d::dyn::mono<double>)*amount);
+            //d=new d::dou::mono<double>[amount];
+            d=(d::dou::mono<double>*)malloc(sizeof(d::dou::mono<double>)*amount);
             // メルセンヌ・ツイスター法による擬似乱数生成器を、
             // ハードウェア乱数をシードにして初期化
             std::random_device seed_gen;
@@ -20,7 +20,7 @@ namespace d::dou {
             // [-1.0, 1.0)の値の範囲で、等確率に実数を生成する
             std::uniform_real_distribution<> dist1(leftBoundary, rightBoundary);
             for(di i=0; i<amount; ++i) {
-                new(d+i)d::dyn::mono((di)2, 0.0, d::coord<double>({dist1(engine), dist1(engine)}), d::coord<double>({dist1(engine), dist1(engine)}));
+                new(d+i)d::dou::mono((di)2, 0.0, d::coord<double>({dist1(engine), dist1(engine)}), d::coord<double>({dist1(engine), dist1(engine)}));
             }
         }
         ~sys2d() {
@@ -35,17 +35,24 @@ namespace d::dou {
 namespace d::dou::compact { // Focus
     template<di dimension=2, di particles=1224>
     struct sys {
-        d::dyn::compact::mono<double, 2ul, dimension, true> d[particles];
+        d::dou::compact::mono<double, 2ul, dimension, true> d[particles];
         constexpr di amount=particles;
         sys(double leftBoundary=-2., double rightBoundary=2.); //bl, tr as template?
         ~sys() {}
+        d::dou::compact::mono<double, 2ul, dimension, true> operator[](int i) const { return this->d[i]; }
+        d::dou::compact::mono<double, 2ul, dimension, true>& operator[](int i) { return this->d[i]; }
+        d::dou::compact::mono<double, 2ul, dimension, true>& operator()(const d::compact::coord<double, dimension>&);
+        d::dou::compact::mono<double, 2ul, dimension, true> operator()(const d::compact::coord<double, dimension>&) const;
+        d::compact::coord<double, dimension> existence(int i) const;
+        d::compact::coord<double, dimensoin> existence(const d::compact::coord<double, dimension>& i) { return this->existence((*this)(i)); }
         bool stable();
+        bool confined();
     };
 }
 
 namespace d::dou::linked {
     struct sys {
-        d::Karabinerhaken<d::dyn::mono<double>>* d;
+        d::Karabinerhaken<d::dou::mono<double>>* d;
         template<typename F> linked& apply(const F& f) {}
     };
 }
@@ -54,7 +61,7 @@ namespace d::dou::linked::compact {
     template<di dimension=2>
     struct sys {
         di amount=1224;
-        d::Karabinerhaken<d::dyn::compact::mono<double, 2ul, dimension, true>>* d=nullptr;
+        d::Karabinerhaken<d::dou::compact::mono<double, 2ul, dimension, true>>* d=nullptr;
         template<typename F> linked& apply(const F& f) {}
         sys(double leftBoundary=-2., double rightBoundary=2.) {
             // メルセンヌ・ツイスター法による擬似乱数生成器を、
@@ -66,22 +73,22 @@ namespace d::dou::linked::compact {
             // [-1.0, 1.0)の値の範囲で、等確率に実数を生成する
             std::uniform_real_distribution<> dist1(leftBoundary, rightBoundary);
 
-            d::dyn::compact::mono<double, 2ul, dimension, true> firstItem;
+            d::dou::compact::mono<double, 2ul, dimension, true> firstItem;
             for(di j=0; j<dimension; ++j) {
                 firstItem[0][j]=dist1(engine); // Position
                 firstItem[1][j]=dist1(engine); // Velocity
             }
 
-            d::Karabinerhaken<d::dyn::compact::mono<double, 2ul, dimension, true>>* cur=new d::Karabinerhaken<d::dyn::compact::mono<double, 2ul, dimension, true>>(firstItem);
+            d::Karabinerhaken<d::dou::compact::mono<double, 2ul, dimension, true>>* cur=new d::Karabinerhaken<d::dou::compact::mono<double, 2ul, dimension, true>>(firstItem);
             this->d=cur;
 
             for(di i=1; i<amount; ++i) {
-                d::dyn::compact::mono<double, 2ul, dimension, true> nextItem;
+                d::dou::compact::mono<double, 2ul, dimension, true> nextItem;
                 for(di j=0; j<dimension; ++j) {
                     nextItem[0][j]=dist1(engine); // Position
                     nextItem[1][j]=dist1(engine); // Velocity
                 }
-                (new d::Karabinerhaken<d::dyn::compact::mono<double, 2ul, dimension, true>>(nextItem))->insertAfter(cur);
+                (new d::Karabinerhaken<d::dou::compact::mono<double, 2ul, dimension, true>>(nextItem))->insertAfter(cur);
                 cur=cur->tugi;
             }
         }
