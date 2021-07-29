7f012e5a32ac3111eb36b21242c0d9d05cace898
fudepia <fudepia@outlook.jp>
2021-07-29 17:07:20 +0800

    Init dev dyn-syssim.

---
+++ lib/dou/sys/carrier.hh
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace d::dou::sys {
+    struct compact {
+    };
+    struct linked {
+    };
+}
--- lib/dyn/sys.hh
+++ lib/dyn/sys.hh
@@ -2,8 +2,10 @@
 #include"../cls/Karabinerhaken.hh"
 #include"./mono.hh"
 
+namespace d::dyn {
 template<typename T>
 struct sys:d::Karabinerhaken<d::dyn::mono<T>> {
     sys& next() {
     }
+}
 }
