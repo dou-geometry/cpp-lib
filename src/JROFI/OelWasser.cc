9c54abce15accff8fbf9c7a4d0f71efb0820c1d7
fudepia <fudepia@outlook.jp>
2021-07-31 14:41:36 +0800

    Create programs

---
+++ src/JROFI/OelWasser.cc
@@ -0,0 +1,32 @@
+/*     JROFI Öl=Wasser Interface behaviour simulation
+ *     Copyright (C) 2021 Dou-geometry
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Structure:
+ *      Öl
+ *    Wasser
+ *
+ * Forces:
+ *   - Gravity   (downward)
+ *   - Floataion (upward)
+ *   - Existence-Difference Acceleration
+ */
+
+#include"../../lib/dou/sys.hh"
+
+int main() {
+    return 0;
+}
