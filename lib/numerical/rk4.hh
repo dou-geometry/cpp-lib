179ca4fdb5317419d9f1498196eba29b8bbf3643
fudepia <fudepia@outlook.jp>
2021-08-07 21:08:21 +0800

    Add callback support for infinite looping rk4::run()

--- lib/numerical/rk4.hh
+++ lib/numerical/rk4.hh
@@ -1,7 +1,9 @@
 #pragma once
 
+#include"../signal/handle.hh"
 #include"../cls/coord.hh"
 #include"../cls/Karabinerhaken.hh"
+#include"../cls/object.hh"
 #include"../dyn/mono.hh"
 #include"../concepts/tracksTime.hh"
 #include<functional>
@@ -10,18 +12,19 @@
 
 namespace d::numerical::rk4 {
     const double h=1e-4;
+    template<typename X> static void logRes(X);
     template<typename R, typename F>
         concept dynMono = requires(const R &r, const F &f) {
             std::convertible_to<F, std::function<typename std::decay<decltype(r[0])>::type(R)>>;
             {f(r)}->std::same_as<typename std::decay<decltype(r[0])>::type>;
             std::convertible_to<R, typename std::decay<decltype(r[0])>::type>;
         };
-    template<int logTrigger=1, bool runForever=false, typename R, typename F, typename T>
-        requires dynMono<R, F> && std::convertible_to<T, std::function<bool(R)>>
-        d::Karabinerhaken<R>* run(R& cur, const F &f, const T& t, double h=d::numerical::rk4::h);
-    template<int logTrigger=1, bool runForever=false, typename R, typename F, typename T>
-        requires dynMono<R, F> && (!(std::convertible_to<T, std::function<bool(R)>>))
-        R run(R& cur, const F &f, const T tt=0, double h=d::numerical::rk4::h);
+    template<int logTrigger=1, bool runForever=false, typename R, typename F, typename T, typename C=std::function<void(R)>>
+        requires dynMono<R, F> && std::convertible_to<T, std::function<bool(R)>> && std::convertible_to<C, std::function<void(R)>>
+        d::Karabinerhaken<R>* run(R& cur, const F &f, const T& t, double h=d::numerical::rk4::h, C cb=d::numerical::rk4::logRes<R>);
+    template<int logTrigger=1, bool runForever=false, typename R, typename F, typename T, typename C=std::function<void(R)>>
+        requires dynMono<R, F> && (!(std::convertible_to<T, std::function<bool(R)>>)) && std::convertible_to<C, std::function<void(R)>>
+        R run(R& cur, const F &f, const T tt=0, double h=d::numerical::rk4::h, C cb=d::numerical::rk4::logRes<R>);
     template<typename C> d::dyn::mono<C> next();
 }
 
