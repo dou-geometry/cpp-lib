35984a82630fdf5c805f252ca6ba17638edd8c43
fudepia <fudepia@outlook.jp>
2021-08-07 21:11:58 +0800

    Fix extern declaration (forget to add)

--- lib/signal/handle.hh
+++ lib/signal/handle.hh
@@ -5,5 +5,6 @@ namespace d::signal {
     extern volatile sig_atomic_t SIGTERMcaught;
     extern volatile sig_atomic_t SIGUSR1caught;
     extern volatile sig_atomic_t SIGUSR2caught;
+    extern volatile sig_atomic_t SIGHUPcaught;
     void signalHandler(int);
 }
