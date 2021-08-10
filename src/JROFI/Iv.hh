dc7a3773f5ba9ef4790c72d7ea297b26f22f45d2
fudepia <fudepia@outlook.jp>
2021-08-10 23:16:28 +0800

    Add conditional termination functions

--- src/JROFI/Iv.hh
+++ src/JROFI/Iv.hh
@@ -13,4 +13,15 @@
 #define pow23(X) pow(sqrt(X), 3.)
 
 namespace d::Iv {
+
+    static bool low(const d::dyn::compact::mono<double, 1, 1, true>& m) {
+        return (1-m[0][0])<1e-2;
+    }
+    static bool medium(const d::dyn::compact::mono<double, 1, 1, true>& m) {
+        return (1-m[0][0])<2e-4;
+    }
+    static bool high(const d::dyn::compact::mono<double, 1, 1, true>& m) {
+        return (1-m[0][0])<2e-5;
+    }
+
 }
