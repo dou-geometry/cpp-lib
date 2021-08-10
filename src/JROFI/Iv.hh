b871f31b82fe158828c7be85095bbc68133b9000
fudepia <fudepia@outlook.jp>
2021-08-10 23:13:20 +0800

    Move to as a shared library

---
+++ src/JROFI/Iv.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+#include"../../lib/signal/handle.hh"
+#include"../../lib/cls/coord.hh"
+#include"../../lib/cls/compact/coord.hh"
+#include"../../lib/dyn/mono.hh"
+#include"../../lib/numerical/rk4.hh"
+#include <cmath>
+#include<iostream>
+#include<string>
+#include<atomic>
+
+#define pow23(X) pow(sqrt(X), 3.)
+
+namespace d::Iv {
+}
