d323b242c75c3f87732f1560c54664d4aef58fc4
fudepia <fudepia@outlook.jp>
2021-08-10 23:18:11 +0800

    Further data generation

--- src/JROFI/Iv.cc
+++ src/JROFI/Iv.cc
@@ -1,3 +1,16 @@
+#include"./Iv.hh"
+
+d::Iv::funcPoints::funcPoints(bool(*cond)(const d::dyn::compact::mono<double, 1, 1, true>&), double k, double c) {
+    auto dv=[](const d::dyn::compact::mono<double, 1, 1, true>& m) {
+        return d::compact::coord<double, 1>({pow(sqrt(1-pow(m[0], 2.)), 3.)});
+    };
+    d::dyn::compact::mono<double, 1, 1, true> m;
+    this->dK=d::numerical::rk4::run<1, false>(m, dv, cond);
+    auto cur=this->dK;
+    for(cur=cur->tugi; cur!=nullptr; cur=cur->tugi) cur->d[0][0]/=cur->d.t;
+    d::Iv::tMax=cur->d.t;
+}
+
 d::Karabinerhaken<d::dyn::compact::mono<double, 1, 1, true>>* d::Iv::genMoreOnce(bool(*cond)(const d::dyn::compact::mono<double, 1, 1, true>&), double k, double c) {
     auto dv=[](const d::dyn::compact::mono<double, 1, 1, true>& m) {
         return d::compact::coord<double, 1>({pow(sqrt(1-pow(m[0], 2.)), 3.)});
