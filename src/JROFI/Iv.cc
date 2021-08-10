8d13bb8d5c55fc8ba3e2c91108f61b8215e52817
fudepia <fudepia@outlook.jp>
2021-08-10 23:28:39 +0800

    Not looping to last in order to save the `tMax`

--- src/JROFI/Iv.cc
+++ src/JROFI/Iv.cc
@@ -7,7 +7,8 @@ d::Iv::funcPoints::funcPoints(bool(*cond)(const d::dyn::compact::mono<double, 1,
     d::dyn::compact::mono<double, 1, 1, true> m;
     this->dK=d::numerical::rk4::run<1, false>(m, dv, cond);
     auto cur=this->dK;
-    for(cur=cur->tugi; cur!=nullptr; cur=cur->tugi) cur->d[0][0]/=cur->d.t;
+    for(cur=cur->tugi; cur->tugi!=nullptr; cur=cur->tugi) cur->d[0][0]/=cur->d.t;
+    cur->d[0][0]/=cur->d.t;
     d::Iv::tMax=cur->d.t;
 }
 
@@ -18,7 +19,8 @@ d::Karabinerhaken<d::dyn::compact::mono<double, 1, 1, true>>* d::Iv::genMoreOnce
     auto contPt=d::Iv::data()->end();
     d::dyn::compact::mono<double, 1, 1, true> m(contPt->d);
     contPt->tugi=d::numerical::rk4::run<1, false>(m, dv, cond)->tugi;
-    for(; contPt!=nullptr; contPt=contPt->tugi) contPt->d[0][0]/=(contPt->d.t*k);
+    for(; contPt->tugi!=nullptr; contPt=contPt->tugi) contPt->d[0][0]/=(contPt->d.t*k);
+    contPt->d[0][0]/=(contPt->d.t*k);
     d::Iv::tMax.store(contPt->d.t);
     return d::Iv::data();
 }
