e0b2dfefa303d0351d5e6a4373192e253de4a245
fudepia <fudepia@outlook.jp>
2021-08-10 23:14:18 +0800

    Move to as shared-lib

--- src/JROFI/SRv=I.cc
+++ src/JROFI/SRv=I.cc
@@ -1,14 +1,5 @@
-#include"../../lib/signal/handle.hh"
-#include"../../lib/cls/coord.hh"
-#include"../../lib/dyn/mono.hh"
-#include"../../lib/numerical/rk4.hh"
 #include"../../lib/conn/sage/settings.hh"
 #include"../../lib/conn/sage/plot.hh"
-#include <cmath>
-#include<iostream>
-#include<string>
-
-#define pow23(X) pow(sqrt(X), 3)
 
 std::string plot(auto& m, d::conn::sage::settings::files<d::conn::sage::settings::png>& info) {
     std::cout << "Plotting" << std::endl;
@@ -41,15 +32,8 @@ int main() {
     signal(SIGTERM, d::signal::handler);
     signal(SIGHUP, d::signal::handler);
     std::cout <<std::fixed<<std::setprecision(14);
-    auto dv=[](const d::dyn::mono<double, true>& m) {
-        return d::coord<double>({pow(sqrt(1-pow(m[0], 2.)), 3.)});
-    };
-    d::dyn::mono<double, true> m(1ul, d::coord<double>(1));
-    d::obj<double, double> o;
-    d::numerical::rk4::run<12, false>(m, dv, 1224);
-    //d::Karabinerhaken<d::dyn::mono<double, true>>* log=d::numerical::rk4::run<12, false>(m, dv, [](const d::dyn::mono<double, true>& x) { return std::abs(x[0][0]-1.)<1e-2; });// [&o](d::dyn::mono<double, true> n){ // Insert result into o
-    //o.add(n.t, n[0]);
-    //});
+    //d::Iv::genFunc([](const d::dyn::compact::mono<double, 1, 1, true>&m){return m.t>7;});
+    //d::Iv::wait();
 
 
     // When SIGHUP is issued, the generation of database is terminated and enters lookup
@@ -73,7 +57,7 @@ int main() {
     d::conn::sage::settings::files<d::conn::sage::settings::png> gph;
     //std::cout << d::conn::sage::anime(m, anim);
     //std::cout << plot(log, gph);
-    std::cout << plot(m, gph);
+    std::cout << plot(d::Iv::data(), gph);
     std::cout << "Animation:\n"<<anim<<"\nPlot:\n"<<gph<<std::endl;
     double ttt;std::cin>>ttt;
     return 0;
