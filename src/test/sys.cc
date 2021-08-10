b5b15d9336bc9876d76687c142aa217e003a9fa4
fudepia <fudepia@outlook.jp>
2021-08-10 13:30:43 +0800

    Reduce testing sample size

--- src/test/sys.cc
+++ src/test/sys.cc
@@ -5,7 +5,7 @@ int main() {
      * whole system: [-2, 2]
      * inteface: [-1, 1]
      */
-    d::dou::compact::sys<2, 21224> s(-2., 2.);
+    d::dou::compact::sys<2, 24> s(-2., 2.);
     std::cout << "Search" << s(d::compact::coord<double, 2>({1,2})) << std::endl;
     return 0;
 }
