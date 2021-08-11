ca08d10944e940a4ab147eb8685ede6f47519fd1
fudepia <fudepia@outlook.jp>
2021-08-11 17:40:43 +0800

    Temporary implementation of `std::boolean` as `std::boolean-testable` isn't available yet

--- lib/cls/suu.hh
+++ lib/cls/suu.hh
@@ -1,9 +1,32 @@
 #pragma once
 #include<concepts>
 
+namespace std {
+  template<class B>
+  concept boolean =
+    movable<remove_cvref_t<B>> &&
+    requires(const remove_reference_t<B>& b1,
+             const remove_reference_t<B>& b2, const bool a) {
+      { b1 } -> convertible_to<bool>;
+      { !b1 } -> convertible_to<bool>;
+      { b1 && b2 } -> same_as<bool>;
+      { b1 &&  a } -> same_as<bool>;
+      {  a && b2 } -> same_as<bool>;
+      { b1 || b2 } -> same_as<bool>;
+      { b1 ||  a } -> same_as<bool>;
+      {  a || b2 } -> same_as<bool>;
+      { b1 == b2 } -> convertible_to<bool>;
+      { b1 ==  a } -> convertible_to<bool>;
+      {  a == b2 } -> convertible_to<bool>;
+      { b1 != b2 } -> convertible_to<bool>;
+      { b1 !=  a } -> convertible_to<bool>;
+      {  a != b2 } -> convertible_to<bool>;
+    };
+}
+
 namespace d {
     template<typename T>
-        requires std::integral<T> || std::floating_point<T> || std::boolean-testable<T>
+        requires std::integral<T> || std::floating_point<T> || std::boolean<T>
     struct suu {
         bool hoka=false;
         T d;
