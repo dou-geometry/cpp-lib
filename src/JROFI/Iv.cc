3cd32ea303a36eef0a478ff70dfcbca52b35d468
fudepia <fudepia@outlook.jp>
2021-08-11 16:46:26 +0800

    Save a copy of v(t) before division by t

--- src/JROFI/Iv.cc
+++ src/JROFI/Iv.cc
@@ -8,6 +8,8 @@ d::IvBackend::funcPoints::funcPoints(bool(*cond)(const d::dyn::compact::mono<dou
     this->dK=d::numerical::rk4::run<1, false>(m, dv, cond);
     auto cur=this->dK;
     for(cur=cur->tugi; cur->tugi!=nullptr; cur=cur->tugi) cur->d[0][0]/=cur->d.t;
+    cur->d.log=(d::dyn::compact::mono<double, 1, 1, true>*)malloc(sizeof(cur->d));
+    new(cur->d.log)d::dyn::compact::mono<double, 1, 1, true>(cur->d);
     cur->d[0][0]/=cur->d.t;
     d::IvBackend::tMax=cur->d.t;
 }
@@ -16,10 +18,14 @@ d::Karabinerhaken<d::dyn::compact::mono<double, 1, 1, true>>* d::IvBackend::genM
     auto dv=[](const d::dyn::compact::mono<double, 1, 1, true>& m) {
         return d::compact::coord<double, 1>({pow(sqrt(1-pow(m[0], 2.)), 3.)});
     };
-    auto contPt=d::Iv::data()->end();
-    d::dyn::compact::mono<double, 1, 1, true> m(contPt->d);
+    auto contPt=d::IvBackend::data()->end();
+    d::dyn::compact::mono<double, 1, 1, true> m(*(contPt->d.log));
+    contPt->d.log->~mono();
+    free(contPt->d.log);
     contPt->tugi=d::numerical::rk4::run<1, false>(m, dv, cond)->tugi;
-    for(; contPt->tugi!=nullptr; contPt=contPt->tugi) contPt->d[0][0]/=(contPt->d.t*k);
+    for(contPt=contPt->tugi; contPt->tugi!=nullptr; contPt=contPt->tugi) contPt->d[0][0]/=contPt->d.t;
+    contPt->d.log=(d::dyn::compact::mono<double, 1, 1, true>*)malloc(sizeof(contPt->d));
+    new(contPt->d.log)d::dyn::compact::mono<double, 1, 1, true>(contPt->d);
     contPt->d[0][0]/=(contPt->d.t*k);
     d::IvBackend::tMax.store(contPt->d.t);
     return d::IvBackend::data();
