5f3adb50af40f50381efddf4071b810acd755ea3
fudepia <fudepia@outlook.jp>
2021-08-11 18:11:57 +0800

    Fix template argument (typo due to global search and replace)

--- lib/manip/translate.cc
+++ lib/manip/translate.cc
@@ -2,5 +2,5 @@
 #include"../dyn/mono.hh"
 
 template d::coord<double> d::translate(d::coord<double>, const d::coord<double>&);
-template d::dyn::mono<double, true> d::translate(d::dyn::mono<double, true>, const d::coord<double, true>&);
-template d::dyn::mono<double, false> d::translate(d::dyn::mono<double, false>, const d::coord<double, false>&);
+template d::dyn::mono<double, true> d::translate(d::dyn::mono<double, true>, const d::coord<double>&);
+template d::dyn::mono<double, false> d::translate(d::dyn::mono<double, false>, const d::coord<double>&);
