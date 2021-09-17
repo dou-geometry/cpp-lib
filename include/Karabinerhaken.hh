caaebca8d045e6a40b3cbb585dcea14074c4a675
fudepia <fudepia@outlook.jp>
2021-09-17 22:04:16 +0800

    Remove empty constructor as it's not recommended

--- include/Karabinerhaken.hh
+++ include/Karabinerhaken.hh
@@ -8,11 +8,11 @@ namespace d{
         struct Karabinerhaken {
             Karabinerhaken* tugi=nullptr;
             C d;
-            //Karabinerhaken()=delete;
-            Karabinerhaken() {
+            Karabinerhaken()=delete;
+            /*Karabinerhaken() {
                 C tmp;
                 d=tmp;
-            }
+            }*/
             Karabinerhaken(C x, Karabinerhaken* n=nullptr): d(x), tugi(n){}
             ~Karabinerhaken() {
                 d.~C();
