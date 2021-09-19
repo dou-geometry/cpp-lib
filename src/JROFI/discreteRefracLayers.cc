44b1965c687884d625879b1c1f5bfcd69a6707aa
fudepia <fudepia@outlook.jp>
2021-09-19 22:48:25 +0800

    Add lib and finish plot func

--- src/JROFI/discreteRefracLayers.cc
+++ src/JROFI/discreteRefracLayers.cc
@@ -16,19 +16,21 @@ std::string plot(const d::numerical::compact::func1d<double, DATAAMOUNT>& f, d::
     info.scriptf<<"import sys\n";
     info.scriptf<<"from sage.all import *\n";
     info.scriptf<<"def main():\n";
-    info.scriptf<<"    ln=-2\n";
+    info.scriptf<<"    lnc=-2\n";
     info.scriptf<<"    base=0\n";
     info.scriptf<<"    dx=1\n";
     info.scriptf<<"    Gph=Graphics()\n";
-    info.scriptf<<"    for ln in sys.stdin():\n";
-    info.scriptf<<"        ln+=1\n";
+    info.scriptf<<"    for ln in sys.stdin:\n";
+    info.scriptf<<"        ln=ln.rstrip()\n";
+    info.scriptf<<"        lnc+=1\n";
     info.scriptf<<"        d=sage_eval(ln)\n";
-    info.scriptf<<"        if(ln==-1):\n";
+    info.scriptf<<"        if(lnc==-1):\n";
     info.scriptf<<"            base=d[0]\n";
     info.scriptf<<"            dx=d[1]\n";
     info.scriptf<<"            continue\n";
-    info.scriptf<<"        Gph+=point((ln*dx+base, d))\n";
+    info.scriptf<<"        Gph+=point((lnc*dx+base, d))\n";
     info.scriptf<<"    Gph.save(filename=sys.argv[1])\n";
+    info.scriptf<<"main()\n";
     info.dataf <<std::fixed<<std::setprecision(14);
     info.dataf << f.base << ", " << f.dx << "\n";
     for(di i=0; i<DATAAMOUNT; ++i)
@@ -42,21 +44,20 @@ std::string plot(const d::polarmono& m, d::conn::sage::settings::files<d::conn::
     info.scriptf<<"#!/usr/bin/env sage\n";
     info.scriptf<<"import sys\n";
     info.scriptf<<"from sage.all import *\n";
-    info.scriptf<<"from sage.plot.line import Line\n";
     info.scriptf<<"def main():\n";
     info.scriptf<<"    E.<x,y> = EuclideanSpace(2)\n";
     info.scriptf<<"    cartesian = E.cartesian_coordinates()\n";
     info.scriptf<<"    polar.<r,t> = E.polar_coordinates()\n";
-    info.scriptf<<"    xs=[]\n";
-    info.scriptf<<"    ys=[]\n";
-    info.scriptf<<"    for ln in sys.stdin():\n";
+    info.scriptf<<"    pts=[]\n";
+    info.scriptf<<"    for ln in sys.stdin:\n";
+    info.scriptf<<"        ln=ln.rstrip()\n";
     info.scriptf<<"        d=ln.split(\"=\")\n";
-    info.scriptf<<"        x=sage_eval(d[1].split(",")[:-3])\n";
+    info.scriptf<<"        x=sage_eval(d[1][:-3])\n";
     info.scriptf<<"        [curX, curY]=E.coord_change(polar, cartesian)(x[0], x[1])\n";
-    info.scriptf<<"        xs.append(curX)\n";
-    info.scriptf<<"        ys.append(curY)\n";
-    info.scriptf<<"    Gph=Line(xs, ys)\n";
+    info.scriptf<<"        pts.append((curX, curY))\n";
+    info.scriptf<<"    Gph=line(pts)\n";
     info.scriptf<<"    Gph.save(filename=sys.argv[1])\n";
+    info.scriptf<<"main()\n";
     info.dataf <<std::fixed<<std::setprecision(14);
     auto logPtr=m.karaLog;
     for(; logPtr!=nullptr; logPtr=logPtr->tugi)
@@ -84,6 +85,7 @@ std::pair<d::polarcoord, d::polarcoord> runSnell(d::polarmono& m, const d::numer
         // move one step
         prev=m[0];
         m[0]+=m[1]*dt;
+        m.t+=dt;
         // log
         karaLog=(new d::Karabinerhaken<d::polarmono>(m))->insertAfter(karaLog);
         std::cout << m << std::endl;
@@ -95,7 +97,7 @@ int main() {
     double inboundAngle, inboundVel, outboundVel;
     std::cin >> inboundAngle;
 
-    d::numerical::compact::func1d<double, DATAAMOUNT> v(0, 1e-4);
+    d::numerical::compact::func1d<double, DATAAMOUNT> v(1e-4, 0);
     d::polarmono m;
     m[0]=d::polarcoord(singleSideThickness, M_PI/2.)+d::polarcoord(.4, M_PI/2.+inboundAngle);
     m[1]=d::polarcoord(singleSideThickness+.4, -M_PI/2.+inboundAngle);
@@ -104,6 +106,12 @@ int main() {
     d::compact::line inboundRay(m.karaLog->d[0], m.karaLog->tugi->d[0]),
         outboundRay(res, prevOut);
     std::cout << "Intersecting angle="<<(d::deg)(inboundRay.ang(outboundRay))<<std::endl;
+    d::conn::sage::settings::files<d::conn::sage::settings::png> gph;
+    d::conn::sage::settings::files<d::conn::sage::settings::png> fgph;
+    std::cout << "Function v(x): \n"<<fgph<<"\nPath: \n"<<gph<<std::endl;
+    std::cout << plot(v, fgph) << std::endl << plot(m, gph) << std::endl;
+    double t;
+    std::cin >> t;
     //assert(res[0]==outboundCheck[0]);
     return 0;
 }
