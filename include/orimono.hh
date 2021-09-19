4d77442ff30c5085e53c92441c6251be63d6c01b
fudepia <fudepia@outlook.jp>
2021-09-19 19:47:06 +0800

    Cleanup

--- include/orimono.hh
+++ include/orimono.hh
@@ -31,12 +31,6 @@ namespace d {
         polarmono& operator-=(const polarmono&);
         friend polarmono& operator+(polarmono, const polarmono&);
         friend polarmono& operator-(polarmono, const polarmono&);
-        /*
-        friend std::ostream& operator<<(std::ostream& os, const polarmono& m) {
-            std::cout << "x="<<m[0]<<", v="<<m[1];
-            return os;
-        }
-        */
         d::polarcoord& operator[](int);
         d::polarcoord operator[](int) const;
         friend std::ostream& operator<<(std::ostream&, const polarmono&);
