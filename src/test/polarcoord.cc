db887f79f186f65a373ef98436cbd988f7e885b7
fudepia <fudepia@outlook.jp>
2021-09-20 16:54:41 +0800

    Add flags

--- src/test/polarcoord.cc
+++ src/test/polarcoord.cc
@@ -8,7 +8,19 @@ inline bool check(const d::polarcoord& res, const d::compact::coord<double, 2>& 
 }
 
 int main() {
+#ifdef AUTOMATED
     d::polarcoord a(1, 0), b(1, d::deg(60));
+#else
+    d::polarcoord a, b;
+    std::cout << "First polar coordinate:\n\tr=";
+    std::cin >> a[0];
+    std::cout << "\ttheta=";
+    std::cin >> a[1];
+    std::cout << "Second polar coordinate:\n\tr=";
+    std::cin >> b[0];
+    std::cout << "\ttheta=";
+    std::cin >> b[1];
+#endif
     d::compact::coord<double, 2> c(a), d(b);
     std::cout << a << std::endl << b << std::endl;
     if(!check(a+b, c+d)) throw "Incorrect!";
