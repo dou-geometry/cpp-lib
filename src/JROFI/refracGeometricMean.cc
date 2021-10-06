ed2f0139b3b1544828ee2311aeeb520c22a20e1f
fudepia <78615424+fudepia@users.noreply.github.com>
2021-10-06 09:39:49 +0800

    Update refracGeometricMean.cc

--- src/JROFI/refracGeometricMean.cc
+++ src/JROFI/refracGeometricMean.cc
@@ -1,7 +1,11 @@
 template<d::R domain,d::R range>
 inline double GM(double x) {
 #ifdef SANITYCHECK
-    assert()
+    assert(domain.contains(x));
+#endif
+    return range.von()*
+        std::pow(range.zu()/range.von(),
+        (x-domain.von())/domain.span());
 }
 
 int main() {
