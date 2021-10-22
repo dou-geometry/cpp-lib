b4988e393580983bc005c67ce3186b586f357ce7
fudepia <fudepia@outlook.jp>
2021-10-22 16:43:13 +0800

    Update

--- include/plot.hh
+++ include/plot.hh
@@ -15,18 +15,6 @@
 #define ull unsigned long long int
 #define ll long long int
 namespace d::conn::sage {
-    std::string plot(cov&, settings::files<settings::png>&);
-    template<typename D, const char *M>
-        requires d::logIncrPromise<D> && //currently useless
-        d::tracksTime<D> &&
-        d::animeFormat<M>
-        std::string anime(const D&, settings::files<M>&, di shadowFrames=0);
-    template<bool logIncrPromise=false, typename D, const char *M>
-        requires d::tracksTime<d::dyn::mono<D, logIncrPromise>>
-        std::string plot(const d::dyn::mono<D, logIncrPromise>&, settings::files<M>&);
-    std::string cleanup(std::stack<std::string>&);
-    //template<typename D> std::string point(Karabinerhaken<d::coord<D>>&, settings::files&);
-    //template<typename D> std::string line(Karabinerhaken<d::coord<D>>&, settings::files&);
     namespace settings {
         // file Extensions
         static const char png[]="png";
@@ -108,6 +96,18 @@ namespace d::conn::sage {
             };
 
     }
+    std::string plot(cov&, settings::files<settings::png>&);
+    template<typename D, const char *M>
+        requires d::logIncrPromise<D> && //currently useless
+        d::tracksTime<D> &&
+        d::animeFormat<M>
+        std::string anime(const D&, settings::files<M>&, di shadowFrames=0);
+    template<bool logIncrPromise=false, typename D, const char *M>
+        requires d::tracksTime<d::dyn::mono<D, logIncrPromise>>
+        std::string plot(const d::dyn::mono<D, logIncrPromise>&, settings::files<M>&);
+    std::string cleanup(std::stack<std::string>&);
+    //template<typename D> std::string point(Karabinerhaken<d::coord<D>>&, settings::files&);
+    //template<typename D> std::string line(Karabinerhaken<d::coord<D>>&, settings::files&);
 }
 #include"./plot.tt"
 #endif
